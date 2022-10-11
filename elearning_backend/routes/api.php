91953720629df52e5d305b6660410a3d6ad48336
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-11 21:42:29 +0300

    Backend <> update routes of APIs

--- elearning_backend/routes/api.php
+++ elearning_backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -10,5 +12,18 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
-
 });
+
+Route::post('addUser', [AdminController::class, 'addStudentOrInstructor']);
+Route::post('addCourse', [AdminController::class, 'addCourses']);
+Route::post('addStudent', [InstructorController::class, 'addStudent']);
+
+// Route::group(["middleware" => "auth:api"], function () {
+//     Route::post('favorites', [UserController::class, 'getFavorites']);
+//     Route::post('addFavorite', [UserController::class, 'addFavorite']);
+//     Route::post('removeFavorite', [UserController::class, 'removeFavorite']);
+//     Route::post('blockUser', [UserController::class, 'blockUser']);
+//     Route::post('editProfile', [UserController::class, 'editProfile']);
+//     Route::post('getMessages', [UserController::class, 'getMessages']);
+//     Route::post('sendMessage', [UserController::class, 'sendMessage']);
+// });
