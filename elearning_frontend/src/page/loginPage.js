bb1389fa35a233e355f21e422785547497e7a761
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:34:56 +0300

    Frontend<>Login page Initialization

---
+++ elearning_frontend/src/page/loginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/login components/loginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default LoginPage;
