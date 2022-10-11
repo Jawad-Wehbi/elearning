961478161fecbb7f9d328e370c211146ed6ef7d5
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:17:11 +0300

    Backend<>Announcement model update

--- elearning_backend/app/Models/Assignment.php
+++ elearning_backend/app/Models/Assignment.php
@@ -6,14 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\Announcement;
 
 class Assignment extends Eloquent
 {
     use HasFactory;
     protected $connection = 'mongodb';
     protected $collection = 'assignments';
-    protected $fillable = [
-        'Assignment_title',
-        'Assignment_content'
-    ];
+    protected $fillable = ['Assignment_title', 'Assignment_content'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
