e715b5b8783fdc1a4f2cfaca21f7a7856e7165e2
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:30:31 +0300

    Frontend<>Routes Addition

--- elearning_frontend/src/App.js
+++ elearning_frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import LoginPage from "./page/loginPage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
+
+  // const ProtectedRoute = () => {
+  //   let {tokens } = useAuth();
+  //   if (!tokens || !tokens.access_token || tokens.access_token === '') {
+  //       console.log(`Navigate to="/login"`, { tokens });
+  //       return <Navigate to="/login" />;
+  //   }
+  //   return <Outlet />;
+  // };
+
+  // function App() {
+  //   return (
+  //     <>
+  //       <BrowserRouter>
+  //         <Routes>
+  //           <Route exact path="/" element={<ProtectedRoute />}>
+  //             {/* default page */}
+  //             <Route
+  //               exact
+  //               path="/Provider"
+  //               element={
+  //                 <Dashboard>
+  //                   <Providers />
+  //                 </Dashboard>
+  //               }
+  //             />
+  //             <Route
+  //               exact
+  //               path="/NewProvider"
+  //               element={
+  //                 <Dashboard>
+  //                   <NewProvider />
+  //                 </Dashboard>
+  //               }
+  //             />
+  //           </Route>
+
+  //           <LoginPage />
+  //         </Routes>
+  //       </BrowserRouter>
+  //     </>
+  //   );
+
+  // <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
 }
+
 
 export default App;
