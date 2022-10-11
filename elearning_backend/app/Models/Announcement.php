86a4fa348ff53fc074b0a75b215a81926fede869
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:14:38 +0300

    Backend<>Add Announcement model

--- elearning_backend/app/Models/Announcement.php
+++ elearning_backend/app/Models/Announcement.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use App\Models\User;
+use App\Models\Assignment;
+use App\Models\Course;
 
-class Announcements extends Model
+class Announcement extends Eloquent
 {
     use HasFactory;
     protected $connection = 'mongodb';
-    protected $collection = '';
-    protected $fillable = [
-        'Announcement_title',
-        'Announcement_content',
-    ];
+    protected $collection = 'announcements';
+    protected $fillable = ['Announcement_title', 'Announcement_content'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
