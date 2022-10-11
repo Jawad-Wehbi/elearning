d0bacecedbe16184a504d82d7bb16bf97121e580
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:32:52 +0300

    Frontend<>Admin Dashboard Initialization

---
+++ elearning_frontend/src/page/admin/adminDashboardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/login components/loginForm";
+
+const AdminDashboardPage = () => {
+  return (
+    <>
+      <ht />
+    </>
+  );
+};
+
+export default AdminDashboardPage;
