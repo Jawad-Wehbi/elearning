8981d1d58e370fe980bc114051b994505f037877
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-12 00:17:36 +0300

    Backend<>Course model update

--- elearning_backend/app/Models/Course.php
+++ elearning_backend/app/Models/Course.php
@@ -6,14 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use App\Models\User;
+use App\Models\Assignment;
+use App\Models\Announcement;
 
 class Course extends Eloquent
 {
     use HasFactory;
     protected $connection = 'mongodb';
     protected $collection = 'courses';
-    protected $fillable = [
-        'course_title',
-        'course_content'
-    ];
+    protected $fillable = ['course_title', 'course_content'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
