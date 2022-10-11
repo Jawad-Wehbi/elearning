42a769a75383cb4244c4d07ce50c425205567922
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-11 21:41:10 +0300

    Backend <> Adding addCourse API

--- elearning_backend/app/Http/Controllers/AdminController.php
+++ elearning_backend/app/Http/Controllers/AdminController.php
@@ -4,16 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Course;
 
 class AdminController extends Controller
 {
-    public function addingStudentOrInstructor(Request $request){
-
+    public function addStudentOrInstructor(Request $request)
+    {
         $user = new User();
         $user->name = $request->get('name');
         $user->email = $request->get('email');
         $user->password = $request->get('password');
-        $user->usertype = $request->get('usertype');     
+        $user->usertype = $request->get('usertype');
         $user->save();
+        return response()->json([
+            'message'=>'User has been successfully added',
+            'status'=>'201',
+            'user'=>$user
+        ],201);
+    }
+
+    public function addCourses(Request $request)
+    {
+        $course = new Course();
+        $course->course_title = $request->get('course_title');
+        $course->course_content = $request->get('course_content');
+        $course->save();
+        return response()->json([
+            'message'=>'Course has been successfully added',
+            'status'=>'201',
+            'course'=>$course
+        ]);
     }
 }
